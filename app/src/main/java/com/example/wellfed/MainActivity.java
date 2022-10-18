731518cc0ce259c3482b1bbe177f77083834126c
Manpreet <manpreetrahal27@gmail.com>
2022-10-18 15:03:18 -0600

    Setup: Added db to the project.

--- app/src/main/java/com/example/wellfed/MainActivity.java
+++ app/src/main/java/com/example/wellfed/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.wellfed;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "Sample";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
