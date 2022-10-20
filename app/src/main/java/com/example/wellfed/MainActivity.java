3247862fa15d7a0c75e12cbb0f69a308b4004961
AP <akshatpandeymyself@gmail.com>
2022-10-20 17:35:58 -0600

    Fix: Crash below API 32

--- app/src/main/java/com/example/wellfed/MainActivity.java
+++ app/src/main/java/com/example/wellfed/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.wellfed;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
