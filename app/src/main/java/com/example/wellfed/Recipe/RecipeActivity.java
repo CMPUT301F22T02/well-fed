60ad9fccd74d151bfb2fae62d852a46c32e40894
Manpreet <manpreetrahal27@gmail.com>
2022-10-20 02:06:59 -0600

    Chore: UI improvement and added back navigation.

--- app/src/main/java/com/example/wellfed/Recipe/RecipeActivity.java
+++ app/src/main/java/com/example/wellfed/Recipe/RecipeActivity.java
@@ -1,16 +1,39 @@
 package com.example.wellfed.Recipe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.wellfed.R;
 
 public class RecipeActivity extends AppCompatActivity {
+    private ListView ingredientList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        String ingredients[] = {"1 lb Ground Beef", "1 table spoon chili powder", "1 teaspoon ground cumin"};
+        ingredientList = findViewById(R.id.recipe_ingredient_listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.activity_recipe_list, ingredients);
+
+        ingredientList.setAdapter(arrayAdapter);
+
+    }
+
+    // helps to go back to the previous active fragment
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
