1c1b3605a46973db8d3a645a27d4ddd948cd92b0
Madison Bradley <mlbradle@ualberta.ca>
2022-10-20 21:16:06 -0600

    setup: setting up basic entity classes

--- app/src/main/java/com/example/wellfed/entity/Ingredient.java
+++ app/src/main/java/com/example/wellfed/entity/Ingredient.java
@@ -1,4 +1,45 @@
 package com.example.wellfed.entity;
 
-public abstract class Ingredient {
+public class Ingredient {
+    private String category;
+    private String description;
+    private int amount;
+    private String unit;
+
+    public Ingredient(String category, String description) {
+        this.category = category;
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 }
--- app/src/main/java/com/example/wellfed/entity/StoredIngredient.java
+++ app/src/main/java/com/example/wellfed/entity/StoredIngredient.java
@@ -1,4 +1,75 @@
 package com.example.wellfed.entity;
 
+import java.util.Date;
+
 public class StoredIngredient extends Ingredient{
+    private String location;
+    private Date bestBefore;
+    private int amountStored;
+    private String unitStored;
+
+    public StoredIngredient(String category, String description) {
+        super(category, description);
+    }
+
+    public StoredIngredient(String category, String description, Date bestBefore) {
+        super(category, description);
+        this.bestBefore = bestBefore;
+    }
+
+    public StoredIngredient(String category, String description, String location) {
+        super(category, description);
+        this.location = location;
+    }
+
+    public StoredIngredient(String category, String description, String location, Date bestBefore) {
+        super(category, description);
+        this.bestBefore = bestBefore;
+        this.location = location;
+    }
+
+    public StoredIngredient(String category,
+                            String description,
+                            String location,
+                            Date bestBefore,
+                            int amountStored,
+                            String unitStored) {
+        super(category, description);
+        this.location = location;
+        this.bestBefore = bestBefore;
+        this.amountStored = amountStored;
+        this.unitStored = unitStored;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getBestBefore() {
+        return bestBefore;
+    }
+
+    public void setBestBefore(Date bestBefore) {
+        this.bestBefore = bestBefore;
+    }
+
+    public int getAmountStored() {
+        return amountStored;
+    }
+
+    public void setAmountStored(int amountStored) {
+        this.amountStored = amountStored;
+    }
+
+    public String getUnitStored() {
+        return unitStored;
+    }
+
+    public void setUnitStored(String unitStored) {
+        this.unitStored = unitStored;
+    }
 }
