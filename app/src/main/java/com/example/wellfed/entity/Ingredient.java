471725fece3e6dfb95a4f67550e7c6ae6c57369a
Madison Bradley <mlbradle@ualberta.ca>
2022-10-22 00:14:52 -0600

    setup: adding entity classes + javadoc

--- app/src/main/java/com/example/wellfed/entity/Ingredient.java
+++ app/src/main/java/com/example/wellfed/entity/Ingredient.java
@@ -1,44 +1,129 @@
+/**
+ * Ingredient
+ *
+ * Version 1.0
+ *
+ * 21 October, 2022
+ *
+ * Copyright (c) 2022.  0xFFFFFF
+ * All rights reserved.
+ */
+
 package com.example.wellfed.entity;
 
+import java.util.ArrayList;
+
+/**
+ * This class represents an Ingredient which can be used in many ways in a MealPlanning application.
+ *
+ * @version 1.0
+ */
 public class Ingredient {
-    private String category;
+    /**
+     * Holds the description of an Ingredient. This can also be considered to be a title.
+     */
     private String description;
+    /**
+     * Holds an ArrayList of all of the categories associated with one Ingredient.
+     */
+    private final ArrayList<String> categories;
+    /**
+     * Holds the amount of one Ingredient, in units
+     * @see #unit
+     */
     private int amount;
+    /**
+     * Holds the units that an amount of an Ingredient is in
+     * @see #amount
+     */
     private String unit;
 
-    public Ingredient(String category, String description) {
-        this.category = category;
+    /**
+     * Creates a new Ingredient object that represents an Ingredient used for various meal purposes.
+     * @param description The description/title of an Ingredient
+     */
+    public Ingredient(String description) {
         this.description = description;
+        this.categories = new ArrayList<>();
     }
 
-    public String getCategory() {
-        return category;
+    /**
+     * Adds a category (aka tag) to an Ingredient object.
+     * @param category The category to add
+     */
+    public void addCategory(String category) {
+        categories.add(category);
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    /**
+     * Removes a category (aka tag) from an Ingredient object.
+     * @param category The category to remove
+     */
+    public void removeCategory(String category) {
+        categories.remove(category);
     }
 
+    /**
+     * Gets the category at the specified index.
+     * @param index The index where the category is at
+     * @return The String representing the category
+     */
+    public String getCategory(int index) {
+        return categories.get(index);
+    }
+
+    /**
+     * Gets the entire ArrayList of categories.
+     * @return The ArrayList of Strings containing all categories
+     */
+    public ArrayList<String> getCategories() {
+        return categories;
+    }
+
+    /**
+     * Gets the description (aka title) of an Ingredient
+     * @return The String representing the description (aka title) of the Ingredient
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description (aka title) of an Ingredient
+     * @param description The String representing the description (aka title) of the Ingredient
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Gets the amount of an Ingredient
+     * @return An int representing the amount of an ingredient
+     */
     public int getAmount() {
         return amount;
     }
 
+    /**
+     * Sets the amount of an Ingredient
+     * @param amount The amount to set of an Ingredient
+     */
     public void setAmount(int amount) {
         this.amount = amount;
     }
 
+    /**
+     * Gets a String representing the unit of an amount
+     * @return A String representing the unit
+     */
     public String getUnit() {
         return unit;
     }
 
+    /**
+     * Sets the unit of an amount
+     * @param unit A String representing the unit
+     */
     public void setUnit(String unit) {
         this.unit = unit;
     }
