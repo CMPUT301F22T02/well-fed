bd0f447345563aac1729cf49cb5be219103412e9
UAWeiguoJiang <weiguo@ualberta.ca>
2022-10-29 17:56:43 -0600

    UI: Created layout file for shopping cart ingredients for RecyclerView inflation

--- app/src/main/java/com/example/wellfed/shoppingcart/ShoppingCartFragment.java
+++ app/src/main/java/com/example/wellfed/shoppingcart/ShoppingCartFragment.java
@@ -9,20 +9,31 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wellfed.R;
 
+import java.util.ArrayList;
+
 public class ShoppingCartFragment extends Fragment {
+    ArrayList<ShoppingCartIngredient> shoppingCartIngredients;
+    ShoppingCartIngredientAdapter adapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable
             ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        shoppingCartIngredients = new ArrayList<>();
         return inflater.inflate(R.layout.fragment_shopping_cart, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Bundle args = getArguments();
+
+        RecyclerView rvShoppingCart = view.findViewById(R.id.shopping_cart_list);
+
+        adapter = new ShoppingCartIngredientAdapter();
     }
 }
