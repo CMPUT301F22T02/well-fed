9b68ce419ff1e9afd2f3ea915708a54b7c4a61a9
UAWeiguoJiang <weiguo@ualberta.ca>
2022-10-30 00:53:58 -0600

    Fix: Try to fix gradle build failure

--- app/src/main/java/com/example/wellfed/shoppingcart/ShoppingCartFragment.java
+++ app/src/main/java/com/example/wellfed/shoppingcart/ShoppingCartFragment.java
@@ -4,25 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wellfed.R;
 
+import java.util.ArrayList;
+
 public class ShoppingCartFragment extends Fragment {
+    ArrayList<ShoppingCartIngredient> shoppingCartIngredients;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable
             ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        shoppingCartIngredients = new ArrayList<>();
         return inflater.inflate(R.layout.fragment_shopping_cart, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Bundle args = getArguments();
+
+        RecyclerView rvShoppingCart = view.findViewById(R.id.shopping_cart_list);
+
+        ShoppingCartIngredient ingredient1 = new ShoppingCartIngredient("Banana");
+        ingredient1.setUnit("chad");
+        shoppingCartIngredients.add(ingredient1);
+
+        ShoppingCartIngredient ingredient2 = new ShoppingCartIngredient("Anana");
+        ingredient2.setUnit("megachad");
+        shoppingCartIngredients.add(ingredient2);
+
+        ShoppingCartIngredient ingredient3 = new ShoppingCartIngredient("Nana");
+        ingredient3.setUnit("gigachad");
+        shoppingCartIngredients.add(ingredient3);
+
+        ShoppingCartIngredientAdapter adapter = new ShoppingCartIngredientAdapter(shoppingCartIngredients);
+        rvShoppingCart.setAdapter(adapter);
+        rvShoppingCart.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
