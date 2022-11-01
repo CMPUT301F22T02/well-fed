ea6bd66cae31e7873a2f17b2bcdeb73a33623729
Tommy <rosansandanasamy@gmail.com>
2022-11-01 16:03:24 -0600

    Added: unit tests for RecipeDB and RecipeIngredientDB

--- app/src/androidTest/java/com/example/wellfed/RecipeIngredientDBTest.java
+++ app/src/androidTest/java/com/example/wellfed/RecipeIngredientDBTest.java
@@ -1,9 +1,5 @@
 package com.example.wellfed;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
-import android.util.Log;
-
 import com.example.wellfed.recipe.RecipeIngredient;
 import com.example.wellfed.recipe.RecipeIngredientDB;
 
@@ -11,7 +7,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Objects;
-import java.util.concurrent.CountDownLatch;
 
 public class RecipeIngredientDBTest {
     RecipeIngredientDB recipeIngredientDB;
@@ -21,27 +16,23 @@ public class RecipeIngredientDBTest {
         recipeIngredientDB = new RecipeIngredientDB();
     }
 
+    /**
+     * Tests the add and get functionality by adding a RecipeIngredient and getting the document
+     * @throws InterruptedException If a transaction in a RecipeIngredientDB method does not succeed
+     */
     @Test
-    public void testAddandDelRecipeIngredients(){
+    public void testAddRecipeIngredients() throws InterruptedException {
 
         RecipeIngredient testIngredient = new RecipeIngredient();
         testIngredient.setDescription("Description");
-        testIngredient.setAmount(1);
+        testIngredient.setAmount(1.0F);
         testIngredient.setCategory("Test");
         testIngredient.setUnit("TestUnits");
-        try {
-            recipeIngredientDB.addRecipeIngredient(testIngredient);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        recipeIngredientDB.addRecipeIngredient(testIngredient);
 
         RecipeIngredient testIngredientFromDb = null;
 
-        try {
-            testIngredientFromDb = recipeIngredientDB.getRecipeIngredient(testIngredient.getId());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        testIngredientFromDb = recipeIngredientDB.getRecipeIngredient(testIngredient.getId());
 
 
         assert Objects.equals(testIngredient.getId(), testIngredientFromDb.getId());
@@ -50,26 +41,91 @@ public class RecipeIngredientDBTest {
         assert Objects.equals(testIngredient.getDescription(), testIngredientFromDb.getDescription());
         assert Objects.equals(testIngredient.getUnit(), testIngredientFromDb.getUnit());
 
-        testIngredient.setAmount((float) 5.0);
-        try {
-            recipeIngredientDB.updateRecipeIngredient(testIngredient);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-        try {
-            testIngredientFromDb = recipeIngredientDB.getRecipeIngredient(testIngredient.getId());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        assert Objects.equals(testIngredient.getAmount(), testIngredientFromDb.getAmount());
-
-        try {
-            recipeIngredientDB.delIngredient(testIngredient.getId());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        recipeIngredientDB.delIngredient(testIngredient.getId());
     }
 
+    /**
+     * Tests the delete functionality on a Recipe, check that the document was deleted
+     * @throws InterruptedException If a transaction in a RecipeIngredientDB method does not succeed
+     */
+    @Test
+    public void testDelRecipeIngredient() throws InterruptedException{
+
+    }
+
+    /**
+     * Tests the update functionality on a recipe ingredient document should check that the fields are
+     * all changed
+     * @throws InterruptedException If a transaction in a RecipeIngredientDB method does not succeed
+     */
+    @Test
+    public void testUpdateRecipeIngredient() throws InterruptedException{
+        RecipeIngredient testIngredient = new RecipeIngredient();
+        testIngredient.setDescription("Description");
+        testIngredient.setAmount(1.0F);
+        testIngredient.setCategory("Test");
+        testIngredient.setUnit("TestUnits");
+        recipeIngredientDB.addRecipeIngredient(testIngredient);
+
+        RecipeIngredient testIngredientFromDb = recipeIngredientDB.getRecipeIngredient(testIngredient.getId());
+
+
+        assert Objects.equals(testIngredient.getId(), testIngredientFromDb.getId());
+        assert Objects.equals(testIngredient.getAmount(), testIngredientFromDb.getAmount());
+        assert Objects.equals(testIngredient.getCategory(), testIngredientFromDb.getCategory());
+        assert Objects.equals(testIngredient.getDescription(), testIngredientFromDb.getDescription());
+        assert Objects.equals(testIngredient.getUnit(), testIngredientFromDb.getUnit());
+
+        testIngredient.setDescription("Description2");
+        testIngredient.setAmount(5.0F);
+        testIngredient.setCategory("Test2");
+        testIngredient.setUnit("TestUnits2");
+        recipeIngredientDB.updateRecipeIngredient(testIngredient);
+
+        testIngredientFromDb = recipeIngredientDB.getRecipeIngredient(testIngredient.getId());
+
+        assert Objects.equals(testIngredient.getDescription(), testIngredientFromDb.getDescription());
+        assert Objects.equals(testIngredient.getAmount(), testIngredientFromDb.getAmount());
+        assert Objects.equals(testIngredient.getCategory(), testIngredientFromDb.getCategory());
+        assert Objects.equals(testIngredient.getDescription(), testIngredientFromDb.getDescription());
+        assert Objects.equals(testIngredient.getUnit(), testIngredientFromDb.getUnit());
+
+        recipeIngredientDB.delIngredient(testIngredient.getId());
+    }
+
+    /**
+     * Tests the get functionality on a nonexistent recipe ingredient document. Should return null
+     * @throws InterruptedException If a transaction in a RecipeIngredientDB method does not succeed
+     */
+    @Test
+    public void testGetOnNonExistentRecipeIngredient() throws InterruptedException{
+        assert recipeIngredientDB.getRecipeIngredient("-1") == null;
+    }
+
+    /**
+     * Tests the delete functionality on a nonexistent recipe ingredient document
+     * should not throw an interrupt unless the transaction cannot successfully complete
+     * Both delIngredient and delRecipeIngredient.
+     * @throws InterruptedException If a transaction in a RecipeIngredientDB method does not succeed
+     */
+    @Test
+    public void testDeleteOnNonExistentRecipeIngredient() throws InterruptedException{
+        recipeIngredientDB.delRecipeIngredient("-1");
+        recipeIngredientDB.delIngredient("-1");
+    }
+
+    /**
+     * Tests the update functionality when a non existent Recipe Ingredeidnet is updated
+     * we check that the Recipe Ingredient isn't added
+     * @throws InterruptedException If a transaction in a RecipeIngredientDB method does not succeed
+     */
+    @Test
+    public void testUpdateOnNonExistentRecipeIngredient() throws InterruptedException{
+        RecipeIngredient recipeIngredient = new RecipeIngredient();
+        recipeIngredient.setId("-1");
+        recipeIngredientDB.updateRecipeIngredient(recipeIngredient);
+
+        assert recipeIngredientDB.getRecipeIngredient(recipeIngredient.getId()) == null;
+    }
 }
