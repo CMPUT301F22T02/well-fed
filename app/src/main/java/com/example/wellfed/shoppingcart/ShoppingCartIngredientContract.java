5400d6c4208adb5caca892683ac51b17b76bbe72
UAWeiguoJiang <weiguo@ualberta.ca>
2022-11-04 02:11:59 -0600

    Implemented: parseResult in ShoppingCartIngredientContract.java.

--- app/src/main/java/com/example/wellfed/shoppingcart/ShoppingCartIngredientContract.java
+++ app/src/main/java/com/example/wellfed/shoppingcart/ShoppingCartIngredientContract.java
@@ -1,5 +1,6 @@
 package com.example.wellfed.shoppingcart;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Pair;
@@ -15,5 +16,25 @@ public class ShoppingCartIngredientContract extends ActivityResultContract<Shopp
     public Intent createIntent(@NonNull Context context,
                                @Nullable ShoppingCartIngredient shoppingCartIngredient) {
         Intent intent = new Intent(context, ShoppingCartEditActivity.class);
+        intent.putExtra("shoppingCartIngredient", shoppingCartIngredient);
+        return intent;
+    }
+
+    @Override
+    public Pair<String, ShoppingCartIngredient> parseResult(int i, Intent intent) {
+        switch(i) {
+            case Activity.RESULT_OK:
+                if (intent == null) {
+                    return null;
+                }
+                ShoppingCartIngredient shoppingCartIngredient = (ShoppingCartIngredient) intent.getSerializableExtra(
+                        "shoppingCartIngredient");
+                String type = intent.getStringExtra("type");
+                return new Pair<>(type, shoppingCartIngredient);
+            case Activity.RESULT_CANCELED:
+                return new Pair<>("quit", null);
+            default:
+                return null;
+        }
     }
 }
