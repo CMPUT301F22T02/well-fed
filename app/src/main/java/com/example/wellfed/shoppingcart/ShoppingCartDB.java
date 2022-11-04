a7ddab800ba88618602147f939196ac81f67ff81
UAWeiguoJiang <weiguo@ualberta.ca>
2022-11-04 05:27:01 -0600

    Added: Initialize ShoppingCartDB.java.

--- app/src/main/java/com/example/wellfed/shoppingcart/ShoppingCartDB.java
+++ app/src/main/java/com/example/wellfed/shoppingcart/ShoppingCartDB.java
@@ -1,4 +1,29 @@
 package com.example.wellfed.shoppingcart;
 
+import com.example.wellfed.recipe.RecipeIngredientDB;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ShoppingCartDB {
+    /**
+     * Holds an instance of the Firebase Firestore database
+     */
+    private final FirebaseFirestore db;
+    /**
+     * Holds a collection to the Recipes stored in the FireStore db database
+     */
+    final CollectionReference shoppingCartIngredientsCollection;
+    /**
+     * Holds a collection to the Recipe Ingredients stored in the FireStore db database
+     */
+    final ShoppingCartDB shoppingCartIngredientDB;
+
+    /**
+     * Create a RecipeDB object
+     */
+    public ShoppingCartDB(){
+        db = FirebaseFirestore.getInstance();
+        this.shoppingCartIngredientsCollection = db.collection("Shopping Cart Ingredients");
+        shoppingCartIngredientDB = new ShoppingCartDB();
+    }
 }
