d2eb2d804d49b5efa5569269adcc93453bbf1c7b
UAWeiguoJiang <weiguo@ualberta.ca>
2022-11-04 02:06:12 -0600

    Created: ShoppingCartEditActivity.java class.

---
+++ app/src/main/java/com/example/wellfed/shoppingcart/ShoppingCartEditActivity.java
@@ -0,0 +1,4 @@
+package com.example.wellfed.shoppingcart;
+
+public class ShoppingCartEditActivity {
+}
--- app/src/main/java/com/example/wellfed/shoppingcart/ShoppingCartIngredientContract.java
+++ app/src/main/java/com/example/wellfed/shoppingcart/ShoppingCartIngredientContract.java
@@ -1,4 +1,19 @@
 package com.example.wellfed.shoppingcart;
 
-public class ShoppingCartIngredientContract {
+import android.content.Context;
+import android.content.Intent;
+import android.util.Pair;
+
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ShoppingCartIngredientContract extends ActivityResultContract<ShoppingCartIngredient,
+        Pair<String, ShoppingCartIngredient>> {
+    @NonNull
+    @Override
+    public Intent createIntent(@NonNull Context context,
+                               @Nullable ShoppingCartIngredient shoppingCartIngredient) {
+        Intent intent = new Intent(context, ShoppingCartEditActivity.class);
+    }
 }
