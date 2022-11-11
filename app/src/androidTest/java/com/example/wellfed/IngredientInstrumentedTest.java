949fb09c19bc8799d53576a7c8772c83c2c71842
Madison Bradley <mlbradle@ualberta.ca>
2022-11-10 21:49:40 -0700

    bug: can't click save button for ingredient?

--- app/src/androidTest/java/com/example/wellfed/IngredientInstrumentedTest.java
+++ app/src/androidTest/java/com/example/wellfed/IngredientInstrumentedTest.java
@@ -5,23 +5,20 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.contrib.PickerActions.setDate;
-import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
-import static androidx.test.espresso.matcher.ViewMatchers.withChild;
-import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import android.widget.DatePicker;
 
-import androidx.test.espresso.contrib.PickerActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.google.android.material.datepicker.MaterialDatePicker;
 
-import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -49,25 +46,64 @@ public class IngredientInstrumentedTest {
     public void before() {
         closeSoftKeyboard();
         onView(withId(R.id.ingredient_storage_item)).perform(click());
+
     }
 
     /**
-     * Tests adding an Ingredient
+     * Tests adding a complete Ingredient.
      */
     @Test
-    public void testAddAndDeleteIngredient() {
+    public void testAddIngredient() {
+        // testing description input
         onView(withId(R.id.fab)).perform(click());
         onView(withId(R.id.descriptionInputEditText)).perform(clearText());
         onView(withId(R.id.descriptionInputEditText)).perform(typeText("Ground Beef"));
 
         // testing best before input - this should get current day as best before
-        // TODO: find a way to pick from the material date picker?
         onView(withId(R.id.bestBeforeInputEditText)).perform(clearText());
         onView(withId(R.id.bestBeforeInputEditText)).perform(click());
         onView(withText("OK")).perform(click());
 
-        
+        // testing category input
+        onView(withId(R.id.categoryInputEditText)).perform(clearText());
+        onView(withId(R.id.categoryInputEditText)).perform(typeText("Meat"));
 
+        // testing amount input
+        onView(withId(R.id.amountInputEditText)).perform(clearText());
+        onView(withId(R.id.amountInputEditText)).perform(typeText("5"));
 
+        // testing unit input
+        onView(withId(R.id.unitInputEditText)).perform(clearText());
+        onView(withId(R.id.unitInputEditText)).perform(typeText("lb"));
+
+        // testing location input
+        onView(withId(R.id.locationInputEditText)).perform(clearText());
+        onView(withId(R.id.locationInputEditText)).perform(typeText("Freezer"));
+
+        // saving input
+        onView(withId(R.id.ingredient_save_button)).perform(click());
+
+        // finding the ingredient in the RecyclerView
+        onView(withId(R.id.ingredient_storage_list))
+                .perform(RecyclerViewActions.actionOnItem(
+                        hasDescendant(withText("Ground Beef")), click()));
+
+        // checking the correctness of the ingredient by seeing if all text is visible
+        onView(withText("Ground Beef")).check(ViewAssertions
+                        .matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withText("Meat")).check(ViewAssertions
+                .matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withText("5.0")).check(ViewAssertions
+                .matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withText("lb")).check(ViewAssertions
+                .matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withText("Freezer")).check(ViewAssertions
+                .matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        // deleting the ingredient
+        onView(withId(R.id.ingredient_delete_button)).perform(click());
+        onView(withText("OK")).perform(click());
     }
+
+
 }
