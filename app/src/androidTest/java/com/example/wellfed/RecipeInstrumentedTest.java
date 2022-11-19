9d36226a571cf5e70967e6d8376edd849c4c8c63
Madison Bradley <mlbradle@ualberta.ca>
2022-11-19 00:14:53 -0700

    fix: adding ingredient before recipe tests

--- app/src/androidTest/java/com/example/wellfed/RecipeInstrumentedTest.java
+++ app/src/androidTest/java/com/example/wellfed/RecipeInstrumentedTest.java
@@ -7,10 +7,12 @@ import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.pressKey;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import androidx.test.espresso.contrib.RecyclerViewActions;
@@ -27,6 +29,7 @@ import android.view.KeyEvent;
 
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -58,22 +61,59 @@ import org.junit.runner.RunWith;
     /**
      * Setup Recipe test by navigating to RecipeBookFragment
      */
-    @Before public void before() {
+    @Before public void before() throws InterruptedException {
+        // pre-add storage ingredient
+        onView(withId(R.id.ingredient_storage_item)).perform(click());
+
+        // typing description input
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.descriptionInputEditText)).perform(clearText());
+        onView(withId(R.id.descriptionInputEditText)).perform(typeText("Tortilla"));
+        closeSoftKeyboard();
+
+        // typing best before input - this should get current day as best before
+        onView(withId(R.id.bestBeforeInputEditText)).perform(clearText());
+        onView(withId(R.id.bestBeforeInputEditText)).perform(click());
+        onView(withText("OK")).perform(click());
+
+        // typing category input
+        onView(withId(R.id.categoryInputEditText)).perform(clearText());
+        onView(withId(R.id.categoryInputEditText)).perform(typeText("Bread"));
+        closeSoftKeyboard();
+
+        // typing amount input
+        onView(withId(R.id.amountInputEditText)).perform(clearText());
+        onView(withId(R.id.amountInputEditText)).perform(typeText("5"));
+        closeSoftKeyboard();
+
+        // typing unit input
+        onView(withId(R.id.unitInputEditText)).perform(clearText());
+        onView(withId(R.id.unitInputEditText)).perform(typeText("count"));
+        closeSoftKeyboard();
+
+        // typing location input
+        onView(withId(R.id.locationInputEditText)).perform(clearText());
+        onView(withId(R.id.locationInputEditText)).perform(typeText("Breadbox"));
+        closeSoftKeyboard();
+
+        onView(withId(R.id.ingredient_save_button)).perform(click());
+
         onView(withId(R.id.recipe_book_item)).perform(click());
+
+        Intents.init();
     }
 
     /**
      * Test add a recipe and delete a recipe
      */
-    @Test public void testAddAndDeleteValidRecipe(){
-        Intents.init();
+    @Test public void testAddAndDeleteRecipe() throws InterruptedException {
 
         onView(withId(R.id.fab)).perform(click());
         //onView(withId(R.id.recipe_img)).perform(click());
-        onView(withId(R.id.edit_recipe_title)).perform(typeText("Test"));
+        onView(withId(R.id.edit_recipe_title)).perform(typeText("Egg Wrap"));
         closeSoftKeyboard();
 
-        onView(withId(R.id.recipe_prep_time_textEdit)).perform(typeText("1"));
+        onView(withId(R.id.recipe_prep_time_textEdit)).perform(typeText("5"));
         closeSoftKeyboard();
 
         onView(withId(R.id.recipe_no_of_servings_textEdit)).perform(typeText("1"));
@@ -87,30 +127,27 @@ import org.junit.runner.RunWith;
                 .perform(click());
         closeSoftKeyboard();
 
-        onView(withId(R.id.commentsEditText)).perform(typeText("TestComment"));
+        onView(withId(R.id.commentsEditText)).perform(typeText("This breakfast is great for on the go."));
         closeSoftKeyboard();
-
 
         onView(withId(R.id.ingredient_search_btn)).perform(click());
         //pick an ingredient check if recycler view is non empty
-        onView(withId(R.id.ingredient_storage_list)).perform(click());
-        intended(hasComponent(RecipeIngredientEditActivity.class.getName()));
+        onView(withText("Tortilla")).perform(click());
+
         onView(withId(R.id.edit_amountInput)).perform(typeText("1"));
         closeSoftKeyboard();
         onView(withId(R.id.unitInput)).perform(click());
-        onView(withText("lb"))
+        onView(withText("count"))
                 .inRoot(RootMatchers.isPlatformPopup())
                 .perform(click());
-        closeSoftKeyboard();
-        onView(withId(R.id.ingredient_save_button)).perform(click());
 
         //add an ingredient
         onView(withId(R.id.ingredient_add_btn)).perform(click());
-        onView(withId(R.id.edit_descriptionInput)).perform(typeText("TestIngredient"));
+        onView(withId(R.id.edit_descriptionInput)).perform(typeText("Egg"));
         closeSoftKeyboard();
 
         onView(withId(R.id.categoryInput)).perform(click());
-        onView(withText("Fruit"))
+        onView(withText("Protein"))
                 .inRoot(RootMatchers.isPlatformPopup())
                 .perform(click());
         closeSoftKeyboard();
@@ -118,34 +155,32 @@ import org.junit.runner.RunWith;
         onView(withId(R.id.edit_amountInput)).perform(typeText("1"));
         closeSoftKeyboard();
 
-        onView(withId(R.id.unitInput)).perform(click());
-        onView(withText("lb"))
-                .inRoot(RootMatchers.isPlatformPopup())
-                .perform(click());
+        onView(withId(R.id.edit_unitInput)).perform(click());
+        onView(withId(R.id.edit_unitInput)).perform(typeText("count"));
+
         closeSoftKeyboard();
         onView(withId(R.id.ingredient_save_button)).perform(click());
 
         onView(withId(R.id.save_fab)).perform(click());
 
-        onView(withId(R.id.recipe_rv))
-                .perform(RecyclerViewActions.actionOnItem(withText("Test"), click()));
+        Thread.sleep(2000);
+        onView(withText("Egg Wrap")).perform(click());
 
         onView(withId(R.id.recipe_delete_btn)).perform(click());
         onView(withText("Delete")).perform(click());
 
-        onView(withId(R.id.recipe_rv))
-                .check(matches(not(hasDescendant(withText("Test")))));
+        onView(withText("Egg Wrap")).check(doesNotExist());
     }
 
+
     @Test public void testAddOnInvalidRecipe(){
-        Intents.init();
 
         onView(withId(R.id.fab)).perform(click());
 
-        onView(withId(R.id.edit_recipe_title)).perform(typeText("Test"));
+        onView(withId(R.id.edit_recipe_title)).perform(typeText("Egg Wrap"));
         closeSoftKeyboard();
 
-        onView(withId(R.id.recipe_prep_time_textEdit)).perform(typeText("1"));
+        onView(withId(R.id.recipe_prep_time_textEdit)).perform(typeText("5"));
         closeSoftKeyboard();
 
         onView(withId(R.id.recipe_no_of_servings_textEdit)).perform(typeText("1"));
@@ -159,7 +194,7 @@ import org.junit.runner.RunWith;
                 .perform(click());
         closeSoftKeyboard();
 
-        onView(withId(R.id.commentsEditText)).perform(typeText("TestComment"));
+        onView(withId(R.id.commentsEditText)).perform(typeText("This is perfect for on the go."));
         closeSoftKeyboard();
 
         //Try to add without ingredients
