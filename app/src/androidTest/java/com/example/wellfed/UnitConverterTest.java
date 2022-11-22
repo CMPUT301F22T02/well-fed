9d91a80da98cb04a6a487d0ec0a7e55f10f3b2fa
Steven Tang <18170455+steventango@users.noreply.github.com>
2022-11-22 16:27:16 -0700

    test: add unit converter test

---
+++ app/src/androidTest/java/com/example/wellfed/UnitConverterTest.java
@@ -0,0 +1,23 @@
+package com.example.wellfed;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.wellfed.unit.UnitConverter;
+
+public class UnitConverterTest {
+    private static final String TAG = "UnitConverterTest";
+
+    @org.junit.Test public void testConvert() {
+        Context context =
+                InstrumentationRegistry.getInstrumentation().getTargetContext();
+        UnitConverter unitConverter = new UnitConverter(context);
+        Double result =
+                unitConverter.convert(2, "Sugar", unitConverter.build("kg"),
+                        unitConverter.build("metric cup"));
+        assertEquals((Double) 8.421052631578949, result);
+    }
+}
