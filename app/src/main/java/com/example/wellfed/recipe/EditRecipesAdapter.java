7b042a484a23f80b68c3e4955b059cb0038a192f
AP <akshatpandeymyself@gmail.com>
2022-11-22 22:09:35 -0700

    EditRecipesAdapter javadocs

--- app/src/main/java/com/example/wellfed/recipe/EditRecipesAdapter.java
+++ app/src/main/java/com/example/wellfed/recipe/EditRecipesAdapter.java
@@ -4,17 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wellfed.common.EditItemAdapter;
-import com.example.wellfed.ingredient.Ingredient;
 
 
 public class EditRecipesAdapter extends EditItemAdapter<Recipe> {
+    /**
+     * OnBindViewHolder binds the view holder to the data. It sets the text
+     * of the view holder to the name of the recipe.
+     * @param holder1 The view holder to bind.
+     * @param position The position of the Recipe in the list.
+     */
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder1,
                                  int position) {
 //        TODO: this holder type causes strange spaghetti code
         super.onBindViewHolder(holder1, position);
         ItemViewHolder holder = (ItemViewHolder) holder1;
-        Recipe recipe = (Recipe) items.get(position);
+        Recipe recipe = items.get(position);
         holder.getHeadlineTextView().setText(recipe.getTitle());
     }
 }
