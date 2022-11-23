dcdae5538d0889c80557e2dc4a3e139fa8124a99
AP <akshatpandeymyself@gmail.com>
2022-11-22 22:11:13 -0700

    EditRecipesFragment javadocs

--- app/src/main/java/com/example/wellfed/recipe/EditRecipesFragment.java
+++ app/src/main/java/com/example/wellfed/recipe/EditRecipesFragment.java
@@ -4,22 +4,35 @@ import android.content.Intent;
 import android.util.Pair;
 
 import com.example.wellfed.common.EditRecyclerViewFragment;
-import com.example.wellfed.ingredient.Ingredient;
 
 public class EditRecipesFragment extends EditRecyclerViewFragment<Recipe> {
 
+    /**
+     * createOnEditIntent creates an intent to edit a recipe.
+     * @param item The recipe to edit.
+     * @return The intent to edit the recipe.
+     */
     public Intent createOnEditIntent(Recipe item) {
         Intent intent = new Intent(getContext(), RecipeEditActivity.class);
         intent.putExtra("item", item);
         return intent;
     }
 
+    /**
+     * createOnSearchIntent creates an intent to add a recipe.
+     * @param item The recipe to add.
+     * @return The intent to add a recipe.
+     */
     public Intent createOnSearchIntent(Recipe item) {
         Intent intent = new Intent(getContext(), RecipesSearch.class);
         intent.putExtra("item", item);
         return intent;
     }
 
+    /**
+     * onSearchActivityResult is called when the user returns from the search
+     * @param result The result of the search activity.
+     */
     @Override
     public void onSearchActivityResult(Pair<String, Recipe> result) {
         if (result == null) {
