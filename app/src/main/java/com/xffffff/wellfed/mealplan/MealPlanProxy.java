f74d8da86eef9f80dd897ff59f646ab5b4395752
Steven Tang <18170455+steventango@users.noreply.github.com>
2022-11-25 19:11:24 -0700

    bug: hide edit fab if MealPlanProxy

---
+++ app/src/main/java/com/xffffff/wellfed/mealplan/MealPlanProxy.java
@@ -0,0 +1,29 @@
+package com.xffffff.wellfed.mealplan;
+
+/**
+ * ProxyMealPlan class. It is a proxy for the meal plan object, that does not
+ * load the ingredient and recipe references.
+ */
+public class MealPlanProxy extends MealPlan {
+    /**
+     * Creates a new ProxyMealPlan object which represents a meal to be eaten
+     * on a certain date.
+     *
+     * @param title The title of the MealPlan
+     */
+    public MealPlanProxy(String title) {
+        super(title);
+    }
+
+    /**
+     * Get MealPlan
+     */
+    public MealPlan getMealPlan() {
+        MealPlan mealPlan = new MealPlan(this.getTitle());
+        mealPlan.setId(this.getId());
+        mealPlan.setCategory(this.getCategory());
+        mealPlan.setEatDate(this.getEatDate());
+        mealPlan.setServings(this.getServings());
+        return mealPlan;
+    }
+}
