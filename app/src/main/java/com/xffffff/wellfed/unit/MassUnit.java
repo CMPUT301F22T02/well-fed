c5149ecea0d974fd892d6b683dfa8162a8085fa2
AP <akshatpandeymyself@gmail.com>
2022-11-25 21:39:35 -0700

    MassUnit javadocs

--- app/src/main/java/com/xffffff/wellfed/unit/MassUnit.java
+++ app/src/main/java/com/xffffff/wellfed/unit/MassUnit.java
@@ -4,19 +4,39 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * MassUnit is a class that contains all the mass units that are supported by
+ * the app
+ */
 public class MassUnit extends Unit {
 
+    /**
+     * The smallest unit for metric mass
+     */
     public final static String smallestMetricUnit = "g";
+    /**
+     * The smallest unit for imperial mass
+     */
     public final static String smallestImperialUnit = "oz";
 
+    /**
+     * The conversion factors for the mass units
+     */
     public final static HashMap<String, Double> CONVERSION_FACTORS =
             new HashMap<>(Map.of("g", 1d, "kg", 1000d, "oz", 28.349523125, "lb",
                     453.59237));
+    /**
+     * The unit systems that the mass units are in
+     */
     public static final HashMap<String, Set<String>> UNIT_SYSTEMS =
             new HashMap<>(
                     Map.of("g", Set.of("metric"), "kg", Set.of("metric"), "oz",
                             Set.of("imperial"), "lb", Set.of("imperial")));
 
+    /**
+     * Constructor for the MassUnit class
+     * @param unit the unit of mass
+     */
     public MassUnit(String unit) {
         super(unit);
         if (!CONVERSION_FACTORS.containsKey(unit)) {
